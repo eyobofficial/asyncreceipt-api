178c3230f8febc195e227becfcfcbbdcaec9f208
Eyob Tariku <eyobtariku@gmail.com>
2021-03-22 15:07:41 +0300

    Add `heroku.py` setting module

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -7,16 +7,8 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # POSTRESQL DATABASE
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT')
-    }
-}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # AWS S3
